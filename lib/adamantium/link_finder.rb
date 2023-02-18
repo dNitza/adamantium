c92428617cd83d80085c85e934da651200c8ee8c
Daniel Nitsikopoulos <daniel@dnitza.com>
2023-02-18 16:53:46 +1100

    fixup! Add web mentions

---
+++ lib/adamantium/link_finder.rb
@@ -0,0 +1,21 @@
+require "uri"
+
+module Adamantium
+  class LinkFinder
+    IGNORE_LIST = %w[twitter.com
+      instagram.com
+      google.com
+      overcast.fm
+      youtube.com
+      github.com]
+
+    def call(content)
+      links = URI::DEFAULT_PARSER.extract(content)
+
+      links.reject do |link|
+        link.end_with?(":") ||
+          IGNORE_LIST.include?(URI.parse(link).host)
+      end
+    end
+  end
+end
