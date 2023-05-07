9ef9eda41c8fe189537ccfcd2608acbc0b9e6cb8
Daniel Nitsikopoulos <daniel@dnitza.com>
2023-05-07 20:53:20 +1000

    Fix reverse cron ordering

--- slices/admin/views/photos/index.rb
+++ slices/admin/views/photos/index.rb
@@ -6,7 +6,7 @@ module Admin
         MEDIA_DIR = "public/media/".freeze
 
         expose :photos_buckets do
-          Dir["#{MEDIA_DIR}*"].each_with_object({}) do |root, memo|
+          Dir["#{MEDIA_DIR}*"].reverse.each_with_object({}) do |root, memo|
             memo[root.gsub(MEDIA_DIR, "")] = Dir["#{root}/**"]
           end
         end
