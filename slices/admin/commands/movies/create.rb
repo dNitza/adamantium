edd7812e5c4c543b394ecc579c40a9e4c64a80f5
Daniel Nitsikopoulos <daniel@dnitza.com>
2023-06-14 19:27:32 +1000

    Fix movie ingestion

--- slices/admin/commands/movies/create.rb
+++ slices/admin/commands/movies/create.rb
@@ -9,9 +9,9 @@ module Admin
         def call(movie)
           repo = Adamantium::Container["repos.movie_repo"]
 
-          return if repo.by_title_and_year(title: title, year: activity.year)
+          return if repo.by_title_and_year(title: movie[:title], year: movie[:year])
 
-          page = Down.download(activity.film_link)
+          page = Down.download(movie[:url])
           match = page.read.match(/href=".+title\/(tt\d+)\/maindetails"/)
           imdb_id = match[1]
 
