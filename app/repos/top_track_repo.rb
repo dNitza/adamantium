85dd61510ec891c5052fba40e062cf5b4977ba3f
Daniel Nitsikopoulos <daniel@dnitza.com>
2023-06-15 20:22:07 +1000

    [standard] fix violations

--- app/repos/top_track_repo.rb
+++ app/repos/top_track_repo.rb
@@ -10,7 +10,7 @@ module Adamantium
       def upsert(post_id:, name:, artist:, url:, mb_id:)
         top_tracks
           .upsert({name: name, artist: artist, url: url, mb_id: mb_id, post_id: post_id},
-                          { target: :post_id, update: {name: name, artist: artist, url: url, mb_id: mb_id, post_id: post_id} })
+            {target: :post_id, update: {name: name, artist: artist, url: url, mb_id: mb_id, post_id: post_id}})
       end
     end
   end
