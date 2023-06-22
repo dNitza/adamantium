a0bf6ab3dafddb69534868e7b38aaad54e641d16
Daniel Nitsikopoulos <daniel@dnitza.com>
2023-06-22 21:45:33 +1000

    Cleaning up media page

--- slices/admin/views/photos/index.rb
+++ slices/admin/views/photos/index.rb
@@ -3,10 +3,19 @@ module Admin
     module Photos
       class Index < Admin::View
         MEDIA_DIR = "public/media/".freeze
+        IGNORE_PATHS = ["podcast_art"].freeze
 
-        expose :photos_buckets do
-          Dir["#{MEDIA_DIR}*"].reverse.each_with_object({}) do |root, memo|
+        expose :photos_buckets do |available_paths|
+          available_paths.each_with_object({}) do |root, memo|
             memo[root.gsub(MEDIA_DIR, "")] = Dir["#{root}/**"]
+          end
+        end
+
+        private_expose :available_paths do
+          Dir["#{MEDIA_DIR}*"]
+            .reverse
+            .reject do |path|
+              IGNORE_PATHS.any?{|ip| path.match(ip) }
           end
         end
       end
