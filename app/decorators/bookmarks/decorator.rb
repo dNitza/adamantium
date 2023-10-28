6195f3b9473cecce70f02b5d8087b7766acbfe05
Daniel Nitsikopoulos <daniel@dnitza.com>
2023-10-28 20:59:52 +1100

    Embed youtube videos in bookmarks

--- app/decorators/bookmarks/decorator.rb
+++ app/decorators/bookmarks/decorator.rb
@@ -30,6 +30,16 @@ module Adamantium
             }
           end
         end
+
+        def youtube_embed
+          pattern = /watch[?]v=(\w*)/i
+          code = url.scan(pattern).flatten.first
+          if code
+            "<div class='video-container'><iframe width='100%' src='https://www.youtube.com/embed/#{code}' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' allowfullscreen></iframe></div>"
+          else
+            nil
+          end
+        end
       end
     end
   end
