07b0b92da67d79215860487c1dfe07280d527618
Daniel Nitsikopoulos <daniel@dnitza.com>
2023-11-18 12:06:39 +1100

    Move caching in to main action

--- app/actions/recently_played/index.rb
+++ app/actions/recently_played/index.rb
@@ -4,10 +4,10 @@ module Adamantium
   module Actions
     module RecentlyPlayed
       class Index < Action
-        include Deps["views.recently_played.index", "view_cache.cacher"]
+        include Deps["views.recently_played.index"]
 
         def handle(req, res)
-          res.body = cacher.call(key: "recently_played", content: index.call.to_str, expiry: TimeMath.min.advance(Time.now, +10))
+          res.body = cache(key: "recently_played", content: index.call.to_str)
         end
       end
     end
