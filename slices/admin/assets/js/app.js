92c435c1ed29870ec0c6e2e8441d1706a5ceb0df
Daniel Nitsikopoulos <daniel@dnitza.com>
2023-12-02 20:47:33 +1100

    Fix admin assets

---
+++ slices/admin/assets/js/app.js
@@ -0,0 +1,23 @@
+import "../../../../app/assets/builds/tailwind.css";
+import "../css/app.css";
+
+import TinyMDE from "tiny-markdown-editor";
+import defo from "@icelab/defo";
+
+(function() {
+    document.addEventListener("DOMContentLoaded", function () {
+        const views = {
+            markdown: (el, attrs) => {
+
+                var tinyMDE = new TinyMDE.Editor({textarea: attrs.editorId});
+
+                return {
+                    update: (newName, oldName) => {},
+                    destroy: () => {}
+                };
+            }
+        };
+
+        defo({views});
+    });
+})();
