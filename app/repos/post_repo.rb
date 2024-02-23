507addb47077778f3427bc32b736474a1ad1ed1c
Daniel Nitsikopoulos <daniel@dnitza.com>
2024-02-23 18:01:59 +1100

    remove sequel extension from root post_repo

--- app/repos/post_repo.rb
+++ app/repos/post_repo.rb
@@ -1,9 +1,6 @@
 module Adamantium
   module Repos
     class PostRepo < Adamantium::Repo[:posts]
-      Sequel.extension :pg_json
-      Sequel.extension :pg_json_ops
-
       def for_reminders(limit:)
         posts
           .where(post_type: "bookmark", is_read: false)
