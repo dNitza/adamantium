b6ccc90b07a85ab29d5e37af51de6baf99f00b67
Daniel Nitsikopoulos <daniel@dnitza.com>
2024-02-25 09:07:05 +1100

    Remove tiny-markdown-edditor

--- slices/admin/assets/js/app.js
+++ slices/admin/assets/js/app.js
@@ -1,29 +1,10 @@
 import "../../../../slices/main/assets/builds/app.css";
 import "../css/app.css";
 
-import TinyMDE from "tiny-markdown-editor";
-import defo from "@icelab/defo";
-
 (function() {
     document.addEventListener('alpine:init', () => {
         Alpine.magic('clipboard', () => {
             return subject => navigator.clipboard.writeText(subject)
         })
     })
-
-    document.addEventListener("DOMContentLoaded", function () {
-        const views = {
-            markdown: (el, attrs) => {
-
-                var tinyMDE = new TinyMDE.Editor({textarea: attrs.editorId});
-
-                return {
-                    update: (newName, oldName) => {},
-                    destroy: () => {}
-                };
-            }
-        };
-
-        defo({views});
-    });
 })();
