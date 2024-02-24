f9a4baa45faddb76d5176e969ff5fa27eb6f3ed3
Daniel Nitsikopoulos <daniel@dnitza.com>
2024-02-24 15:02:10 +1100

    Fix json parsing

--- slices/micropub/request_parser.rb
+++ slices/micropub/request_parser.rb
@@ -53,7 +53,7 @@ module Micropub
 
       new_params = if req_type == :json
          content = if params[:properties][:content]
-                     if params[:properties][:content].is_a?(Hash) && params[:properties][:content][:html]
+                     if params[:properties][:content].is_a?(Array) && params[:properties][:content].first[:html]
                        params[:properties][:content][:html]
                      else
                        params[:properties][:content].first&.tr("\n", " ")
