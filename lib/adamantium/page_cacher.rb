6a5ff26948058b919a565985b9c971220add18a5
Daniel Nitsikopoulos <daniel@dnitza.com>
2024-03-18 19:33:58 +1100

    Update readability doc tags

--- lib/adamantium/page_cacher.rb
+++ lib/adamantium/page_cacher.rb
@@ -5,7 +5,7 @@ module Adamantium
   class PageCacher
     def call(url:, &block)
       tempfile = Down.download(url)
-      content = Readability::Document.new(tempfile.read, tags: %w[div section header p h1 h2 h3 h4 h5 h6 ol ul li table td tr thead tbody a code pre], attributes: %w[href]).content
+      content = Readability::Document.new(tempfile.read, tags: %w[div section header p h1 h2 h3 h4 h5 h6 ol ul li table td tr thead tbody a code pre article], attributes: %w[href]).content
       yield content
     end
   end
