6cabe288fc6d626e377fbfd2bc7b12c260bdb7ae
Daniel Nitsikopoulos <daniel@dnitza.com>
2024-08-23 20:44:53 +1000

    Default main slice headers

--- slices/main/action.rb
+++ slices/main/action.rb
@@ -3,5 +3,14 @@
 
 module Main
   class Action < Adamantium::Action
+    before :default_headers
+
+    private
+
+    def default_headers(_, res)
+      res.headers["Strict-Transport-Security"] = "max-age=3600"
+      res.headers["Referrer-Policy"] = "origin"
+      res.headers["Permissions-Policy"] = "microphone=(), geolocation=()"
+    end
   end
 end
